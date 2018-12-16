5acc55ae821696a73c20e35423af3cd62655abd8
Alexander Zaak <alexander.zaak@gmail.com>
2018-12-16 13:33:43 +0100

    added models

---
+++ ArchitectureSample/app/src/main/java/codes/zaak/architecturesample/repository/model/Character.kt
@@ -0,0 +1,3 @@
+package codes.zaak.architecturesample.repository.model
+
+data class Character(val id: Int, val name: String, val image: String, val power: String, val race: String, val saga: String)
---
+++ ArchitectureSample/app/src/main/java/codes/zaak/architecturesample/repository/model/Saga.kt
@@ -0,0 +1,3 @@
+package codes.zaak.architecturesample.repository.model
+
+data class Saga(val id: Int, val name: String, val characters: List<Character>, val description: String, val image: String)
