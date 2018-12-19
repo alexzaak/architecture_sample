1db873e73ab6867a87f2cc7a524d29e6a9b5b503
Alexander Zaak <alexander.zaak@gmail.com>
2018-12-19 22:17:44 +0100

    ad recyclerview and adapter

--- ArchitectureSample/app/src/main/java/codes/zaak/architecturesample/MainActivity.kt
+++ ArchitectureSample/app/src/main/java/codes/zaak/architecturesample/MainActivity.kt
@@ -1,34 +1,77 @@
 package codes.zaak.architecturesample
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.*
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import codes.zaak.architecturesample.view.CharacterAdapter
 import codes.zaak.architecturesample.viewmodel.AppViewModelFactory
 import codes.zaak.architecturesample.viewmodel.MainViewModel
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
 
     @Inject
     lateinit var viewModelFactory: AppViewModelFactory
     private lateinit var viewModel: MainViewModel
+
+    var characterAdapter = CharacterAdapter(ArrayList())
+
+    lateinit var mLifecycleRegistry: LifecycleRegistry
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidInjection.inject(this)
         setContentView(R.layout.activity_main)
 
+        mLifecycleRegistry = LifecycleRegistry(this)
+        mLifecycleRegistry.markState(Lifecycle.State.CREATED)
+
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
 
         viewModel.loadCharacters()
 
-        viewModel.characterResult().observe(this, Observer {
+        viewModel.characterResult.observe(this, Observer {
             Timber.d(it.toString())
-            print(it.toString())
+            if (it != null) {
+                characterAdapter.addCharacterList(it)
+                recycler.adapter = characterAdapter
+            }
         })
+
+        viewModel.characterError.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        })
+
+        viewModel.characterLoader.observe(this, Observer {
+            Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+        })
+
+        val gridLayoutManager = GridLayoutManager(this, 1)
+        gridLayoutManager.orientation = RecyclerView.VERTICAL
+        recycler.apply {
+            setHasFixedSize(true)
+            layoutManager = gridLayoutManager
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        mLifecycleRegistry.markState(Lifecycle.State.STARTED)
+    }
+
+    override fun getLifecycle(): Lifecycle {
+        return mLifecycleRegistry
+    }
+
+    override fun onDestroy() {
+        viewModel.disposeElements()
+        super.onDestroy()
     }
 }
