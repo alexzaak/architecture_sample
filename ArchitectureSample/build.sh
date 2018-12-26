d7268d081fcf512d8e3eddd8dd29b5ef96881601
Alexander Zaak <alexander.zaak@gmail.com>
2018-12-26 16:23:10 +0100

    fix build

--- ArchitectureSample/build.sh
+++ ArchitectureSample/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+cd ~/
 export ANDROID_HOME="~/android-sdk"
 export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
+cd architecture_sample/ArchitectureSample/
 chmod a+x ./gradlew
 ./gradlew publish
